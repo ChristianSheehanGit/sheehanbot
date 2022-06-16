74fe852b71f96b6fd043da6edb6bc408a4f975a7
Christian Sheehan <53072815+ItsAllCalculated@users.noreply.github.com>
2022-06-16 11:31:01 -0500

    Update sheehanbot.py

--- sheehanbot.py
+++ sheehanbot.py
@@ -1,8 +1,8 @@
-import discord
-import random
-from discord.ext import commands
+# import discord
+# import random
+# from discord.ext import commands
 
-bot = commands.Bot(command_prefix='.')
+# bot = commands.Bot(command_prefix='.')
 bot.remove_command("help")
 
 @bot.command()
